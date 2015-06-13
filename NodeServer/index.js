41b61f5e985db43160f48e7414a0b62e4a0085bd
George Sapp <george@internetthings.io>
2015-06-12 22:24:57 -0400

    Adding functionality to check user token.  Logging first

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var xhr = require('xhr2');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/admin.html');
@@ -12,11 +13,13 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
   });
 
-  socket.on('join room', function(roomID){
-    socket.join(roomID);
+  socket.on('join room', function(token){
+    console.log('Trying to join room, token: ' + token);
+    checkToken(token, joinRoom);
+
   });
 
-  socket.on('leave room', function(roomID){
+  socket.on('leave room', function(token, roomID){
     socket.leave(roomID);
   });
 
@@ -36,3 +39,37 @@ io.on('connection', function(socket){
 http.listen(80, function(){
   console.log('listening on *:80');
 });
+
+//gets user's info
+function checkToken(access_token, callback) {
+
+
+  requestStart();
+
+  function requestStart() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://www.googleapis.com/plus/v1/people/me');
+    xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
+    xhr.onload = requestComplete;
+    xhr.send();
+  }
+
+  function requestComplete() {
+    if (this.status == 200) {
+      console.log('Success ' + this.response);
+      //var user_info = JSON.parse(this.response);
+
+
+
+      //callback(null, this.status, this.response);
+    }else{
+      console.log('(maybe token is bad?) Auth failed with status: ' + this.status + 'response: ' + this.response);
+    }
+  }
+}
+
+function joinRoomfunction(email) {{
+
+  socket.join(roomID);
+
+}
