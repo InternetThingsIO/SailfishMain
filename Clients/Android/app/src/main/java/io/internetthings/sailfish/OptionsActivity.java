ee57e9cb3822ea505219814b1f4d029c2b3167f8
Jason <jason@internetthings.io>
2015-07-15 22:36:05 -0400

    Added buttons to theme, added a few things the main activity and options

--- Clients/Android/app/src/main/java/io/internetthings/sailfish/OptionsActivity.java
+++ Clients/Android/app/src/main/java/io/internetthings/sailfish/OptionsActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import io.internetthings.sailfish.ftue.ConfigureChromeActivity;
 import io.internetthings.sailfish.ftue.SelectEmailActivity;
