c768abf54394d55f09e63eb1a3dea8f9d2270874
Jason <jason@internetthings.io>
2015-07-16 00:20:14 -0400

    User can now change email via popup. Added Toast message that shows email address that chrome setup email was sent to when the Add a Device button was pressed

--- Clients/Android/app/src/main/java/io/internetthings/sailfish/OptionsActivity.java
+++ Clients/Android/app/src/main/java/io/internetthings/sailfish/OptionsActivity.java
@@ -1,16 +1,20 @@
 package io.internetthings.sailfish;
 
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+
+import com.google.android.gms.common.AccountPicker;
 
 import io.internetthings.sailfish.ftue.ConfigureChromeActivity;
 import io.internetthings.sailfish.ftue.SelectEmailActivity;
 
 
 public class OptionsActivity extends Activity {
+
+    static final int PICK_ACCOUNT_REQUEST =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +27,31 @@ public class OptionsActivity extends Activity {
     }
 
     public void changeACCTEmail(View view){
-        Intent i = new Intent(this, SelectEmailActivity.class);
-        startActivity(i);
+        Intent i = AccountPicker.newChooseAccountIntent(null, null, new String[]{"com.google"},
+                false, null, null, null, null);
+        startActivityForResult(i, PICK_ACCOUNT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode,
+                                    final Intent data) {
+        if (requestCode == PICK_ACCOUNT_REQUEST && resultCode == RESULT_OK) {
+            String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+            SailfishPreferences.editor(this).putString(SailfishPreferences.EMAIL_KEY, accountName);
+            SailfishPreferences.editor(this).commit();
+            NotificationActions.toastMSG(getApplication(), "Set to: " + accountName);
+        }
     }
 
     public void sendChromeSetupEmail(View view){
         ConfigureChromeActivity email = new ConfigureChromeActivity();
         email.sendEmail();
+        NotificationActions.toastMSG(getApplication(), "Sent email to: " + SailfishPreferences.reader(this).getString(SailfishPreferences.EMAIL_KEY, null));
+    }
+
+    public void restartSetup(View view){
+        Intent i = new Intent(this, SelectEmailActivity.class);
+        startActivity(i);
     }
 
     public void backToMain(View view){
