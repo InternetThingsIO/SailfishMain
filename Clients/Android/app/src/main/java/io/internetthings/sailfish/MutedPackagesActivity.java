f3928ce6ed5aa42ebd2a8efcd29fddb04b645680
Jason <jason@internetthings.io>
2015-07-30 18:37:47 -0400

    cleaned up muted packages, fixed text alignment, added space between text and drawable, added column names(App Name and Muted), and checkboxes change true/false on click

--- Clients/Android/app/src/main/java/io/internetthings/sailfish/MutedPackagesActivity.java
+++ Clients/Android/app/src/main/java/io/internetthings/sailfish/MutedPackagesActivity.java
@@ -3,28 +3,15 @@ package io.internetthings.sailfish;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.CheckBox;
 import android.widget.CheckedTextView;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
-import android.widget.TableRow;
-import android.widget.TextView;
-
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
 
 public class MutedPackagesActivity extends Activity {
 
@@ -46,7 +33,7 @@ public class MutedPackagesActivity extends Activity {
         Iterator<String> it = mutedPackages.keySet().iterator();
         Log.i("itsize: ", Integer.toString(mutedPackages.size()));
         while(it.hasNext()){
-            String pkg = it.next();
+            final String pkg = it.next();
             boolean value = mutedPackages.get(pkg);
             ApplicationInfo appInfo;
             try{
@@ -58,17 +45,22 @@ public class MutedPackagesActivity extends Activity {
             }
 
             LinearLayout ll = (LinearLayout) findViewById(R.id.checkBoxLL);
+            final CheckedTextView chkBox = new CheckedTextView(this);
 
-            CheckedTextView chkBox = new CheckedTextView(this);
             chkBox.setChecked(value);
             chkBox.setCheckMarkDrawable(R.drawable.custom_checkbox);
-            chkBox.setText(pkgName);
+            chkBox.setText(" " + pkgName);
+            chkBox.setGravity(0x10);
             chkBox.setCompoundDrawablesWithIntrinsicBounds(icon, null, null, null);
             chkBox.setTextSize(24f);
             chkBox.setTextColor(Color.WHITE);
             chkBox.isClickable();
             chkBox.setOnClickListener(new CheckedTextView.OnClickListener() {
                 public void onClick(View v) {
+                    if(chkBox.isChecked())
+                        unMutePackage(pkg);
+                    else
+                        mutePackage(pkg);
                     ((CheckedTextView) v).toggle();
                 }
             });
@@ -77,11 +69,13 @@ public class MutedPackagesActivity extends Activity {
     }
 
     public void mutePackage(String pkg){
-       mutedPackages.put(pkg, true);
+        mutedPackages.put(pkg, true);
+        Log.i(pkg, "true");
     }
 
     public void unMutePackage(String pkg){        
         mutedPackages.put(pkg, false);
+        Log.i(pkg, "false");
     }
 
     public void setMutedPackages(){
