5b988863e01e11f82a9f30ea0b8ee421b1bd5c41
Jason Maderski <jason@internetthings.io>
2015-09-29 13:06:56 -0400

    small code improvements, added method to create 128x128 blank drawable

--- Clients/Android/app/src/main/java/io/internetthings/sailfish/notification/IconWithOverlay.java
+++ Clients/Android/app/src/main/java/io/internetthings/sailfish/notification/IconWithOverlay.java
@@ -11,6 +11,8 @@ import android.graphics.drawable.LayerDrawable;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by Jason on 9/25/15.
  */
@@ -48,17 +50,24 @@ public class IconWithOverlay {
         return new BitmapDrawable(context.getResources(), bitmapResized);
     }
 
-    private Drawable scaleIconLarge(Drawable drawable, Context context){
+    private Drawable scaleIconLarge(Drawable drawable, Context context, int height, int width){
         Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
-        Bitmap bitmapResized = Bitmap.createScaledBitmap(bitmap, 128, 128, false);
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(bitmap, height, width, false);
 
         return new BitmapDrawable(context.getResources(), bitmapResized);
 
     }
 
+    private Drawable blankFixedSizeDrawable(Context context){
+        Bitmap bitmap = Bitmap.createBitmap(128, 128, Bitmap.Config.ARGB_8888);
+
+        return new BitmapDrawable(context.getResources(), bitmap);
+    }
+
     public Drawable overlayedIcon(StatusBarNotification sbn, Context context) {
         Drawable smallIcon = makeIconSmall(getPackageIcon(sbn, context), context);
-        Drawable largeIcon = scaleIconLarge(getNotificationIcon(sbn, context), context);
+        Drawable largeIcon = scaleIconLarge(getNotificationIcon(sbn, context), context, 128, 128);
+        Drawable blankBkGround = blankFixedSizeDrawable(context);
 
         int left = 80;
         int top = 80;
@@ -72,7 +81,10 @@ public class IconWithOverlay {
 
         Drawable[] layers = new Drawable[2];
         layers[0] = largeIcon;
+        //layers[0] = blankBkGround;
         layers[1] = smallIcon;
+
+        //layers[1].setAlpha(0);
 
         LayerDrawable layerDrawable = new LayerDrawable(layers);
         layerDrawable.setLayerInset(1, left, top, right, bottom);
