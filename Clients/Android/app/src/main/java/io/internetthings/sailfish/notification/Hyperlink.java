a75a41ad6a0d42febe33632e695fddaecd9134ca
Jason Maderski <jason@internetthings.io>
2015-10-01 17:59:10 -0400

    Fixed space issue and simplified code...tested and it works!

--- Clients/Android/app/src/main/java/io/internetthings/sailfish/notification/Hyperlink.java
+++ Clients/Android/app/src/main/java/io/internetthings/sailfish/notification/Hyperlink.java
@@ -10,7 +10,10 @@ public class Hyperlink {
     private String logTAG = this.getClass().getName();
 
     public void testHyperlink(){
-        String test = generateHyperLink("Please go to http://www.stackoverflow.com it can be handy to use or check out this article http://www.theverge.com/2015/10/1/9433783/google-self-driving-car-prototype-obstacle-course it's cool!");
+        String test = generateHyperLink("www.canyouseeme.com Please go to www.stackoverflow.com it can be handy to use " +
+                "or check out this article http://www.theverge.com/2015/10/1/9433783/google-self-" +
+                "driving-car-prototype-obstacle-course it's cool!  For more secure site https://" +
+                "www.securetest.com and don't forget to try www.cool.com");
         Log.i(logTAG, test);
     }
 
@@ -18,12 +21,8 @@ public class Hyperlink {
 
         String hyperlink = "";
 
-        if(message.contains("http://"))
-            hyperlink = message.replaceAll("http://.+?( )/{0,1}", "<a href=\"$0\">$0</a>");
-        else if(message.contains("https://"))
-            hyperlink = message.replaceAll("https://.+?( )/{0,1}", "<a href=\"$0\">$0</a>");
-        else if(message.contains("www."))
-            hyperlink = message.replaceAll("www.+?( )/{0,1}", "<a href=\"$0\">$0</a>");
+        hyperlink = message.replaceAll("(http://|https://|www).+?([^\\s]+)/{0,1}", "<a href=\"$0\">$0</a>");
+
 
         return hyperlink;
     }
