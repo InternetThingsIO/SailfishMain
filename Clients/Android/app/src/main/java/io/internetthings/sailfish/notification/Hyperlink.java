999f1b77a0b44d123b25cbcfa03a0f75315e93a9
Jason Maderski <jason@internetthings.io>
2015-10-01 17:15:28 -0400

    Now grabs entire link and stops at space at the end.  Need to figure out how to remove space in the HTML

--- Clients/Android/app/src/main/java/io/internetthings/sailfish/notification/Hyperlink.java
+++ Clients/Android/app/src/main/java/io/internetthings/sailfish/notification/Hyperlink.java
@@ -10,7 +10,7 @@ public class Hyperlink {
     private String logTAG = this.getClass().getName();
 
     public void testHyperlink(){
-        String test = generateHyperLink("Please go to http://www.stackoverflow.com it can be handy to use");
+        String test = generateHyperLink("Please go to http://www.stackoverflow.com it can be handy to use or check out this article http://www.theverge.com/2015/10/1/9433783/google-self-driving-car-prototype-obstacle-course it's cool!");
         Log.i(logTAG, test);
     }
 
@@ -19,11 +19,11 @@ public class Hyperlink {
         String hyperlink = "";
 
         if(message.contains("http://"))
-            hyperlink = message.replaceAll("http://.+?(com|net|org)/{0,1}", "<a href=\"$0\">$0</a>");
+            hyperlink = message.replaceAll("http://.+?( )/{0,1}", "<a href=\"$0\">$0</a>");
         else if(message.contains("https://"))
-            hyperlink = message.replaceAll("https://.+?(com|net|org)/{0,1}", "<a href=\"$0\">$0</a>");
+            hyperlink = message.replaceAll("https://.+?( )/{0,1}", "<a href=\"$0\">$0</a>");
         else if(message.contains("www."))
-            hyperlink = message.replaceAll("www.+?(com|net|org)/{0,1}", "<a href=\"$0\">$0</a>");
+            hyperlink = message.replaceAll("www.+?( )/{0,1}", "<a href=\"$0\">$0</a>");
 
         return hyperlink;
     }
